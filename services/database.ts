ba8793d3d304836554b5e1c42c0978d696df06fa
ronny <ronnydp742@gmail.com>
2026-03-15 21:32:56 -0500

    lista de clientes ordenada alfabeticamente

--- services/database.ts
+++ services/database.ts
@@ -52,7 +52,7 @@ export const getCustomersPaginated = async (
   const to = from + pageSize - 1;
 
   const { data, error, count } = await query
-    .order('created_at', { ascending: false })
+    .order('name', { ascending: true })
     .range(from, to);
 
   if (error) throw error;
